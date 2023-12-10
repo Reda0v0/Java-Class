7725b52ee9010ce28f64930df4639daaa67da5d9
4iir2024 <4iir2024@gmail.com>
2023-12-10 01:46:07 +0100

    M3

---
+++ src/main/iced_Cofeee.java
@@ -0,0 +1,68 @@
+package main;
+
+public class iced_Cofeee {
+	 
+	private String icedCoffeeStrength;
+	private int temperature; 
+	
+	
+
+	public iced_Cofeee() {
+		super();
+	}
+	
+	
+
+
+
+	public iced_Cofeee(String icedCoffeeStrength, int temperature) {
+		super();
+		this.icedCoffeeStrength = icedCoffeeStrength;
+		this.temperature = temperature;
+	}
+
+	
+	
+
+
+
+
+	public String getIcedCoffeeStrength() {
+		return icedCoffeeStrength;
+	}
+
+
+
+
+
+	public void setIcedCoffeeStrength(String icedCoffeeStrength) {
+		this.icedCoffeeStrength = icedCoffeeStrength;
+	}
+
+
+
+
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+
+
+
+
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
+
+
+
+
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
